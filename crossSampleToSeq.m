19941cc6a3f8df124ae477632acad1f9607f6b70
XiyuSun <139063229+XiyuSun@users.noreply.github.com>
2023-07-12 11:38:27 +0800

    Update crossSampleToSeq.m

--- crossSampleToSeq.m
+++ crossSampleToSeq.m
@@ -1,39 +1,3 @@
-% % 交叉取样置乱序列P1
-% % r 轮数
-% % w 偏移量
-% % L 序列长度 
-% % P1 用于置乱的下标序列
-% function [P1, P2] = crossSampleToSeq(k,L)
-%     % 分裂为两个序列    
-%     s1 = 1:floor(L/2);
-%     s2 = floor(L/2)+1:L;
-%     % 间隔2取样
-%     a1 = s1(1:3:end);
-%     a2 = s1(2:3:end);
-%     a3 = s1(3:3:end);  
-%     b1 = s2(1:3:end);
-%     b2 = s2(2:3:end);
-%     b3 = s2(3:3:end);
-%     % 块排列
-%     s = [b1,a1,b2,a2,b3,a3];
-%     % 间隔2取样
-%     s = [s(1:3:end),s(2:3:end),s(3:3:end)];
-%     % 间隔1取样
-%     P = [s(1:2:end),s(2:2:end)];    
-% 
-%     % 自排列
-%     P=P(P);  
-% 
-%     % w1 w2值    
-%     r = length(k); 
-%     k = double(k(:));    
-%     w1 = mod(100*(1:r)*k,L);
-%     w2 = mod(100*(r:-1:1)*k,L);
-%     P1 = circshift(P,-w1);
-%     P2 = circshift(P,-w2);
-% end
-
-
 % 交叉取样置乱序列P1
 % r 轮数
 % w 偏移量
@@ -43,6 +7,7 @@ function [P1, P2] = crossSampleToSeq(k,L)
     % 分裂为两个序列    
     s1 = uint32(1:floor(L/2));
     s2 = uint32(floor(L/2)+1:L);
+    
     % 间隔2取样
     a1 = s1(1:3:end); 
     a2 = s1(2:3:end);
@@ -56,6 +21,7 @@ function [P1, P2] = crossSampleToSeq(k,L)
     lb1 = length(b1);
     lb2 = length(b2);
     lb3 = length(b3);
+    
     % 块排列
     s = zeros(1,L,'uint32');
     st = 1;
@@ -81,7 +47,7 @@ function [P1, P2] = crossSampleToSeq(k,L)
     st = ed + 1; 
     ed = ed + la3;
     s(st:ed) = a3;
-%     s = [b1,a1,b2,a2,b3,a3];
+
     % 间隔2取样
     a1 = s(1:3:end);
     a2 = s(2:3:end);
@@ -102,7 +68,6 @@ function [P1, P2] = crossSampleToSeq(k,L)
     ed = ed + la3;
     s(st:ed) = a3;
 
-%     s = [s(1:3:end),s(2:3:end),s(3:3:end)];
     % 间隔1取样
     a1 = s(1:2:end);
     a2 = s(2:2:end);
@@ -116,8 +81,6 @@ function [P1, P2] = crossSampleToSeq(k,L)
     st = ed + 1; 
     ed = ed + la2;
     s(st:ed) = a2;
-
-%     P = [s(1:2:end),s(2:2:end)];    
 
     % 自排列
     P=s(s);  
